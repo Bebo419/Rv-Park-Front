3dd8472bb57a422f1cddee20926c6609638d2531
Bebo419 <ponce.alm.diego69@gmail.com>
2025-12-06 10:30:24 -0600

    a

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/api',
+        target: 'https://rv-park-backend.onrender.com',
         changeOrigin: true,
         secure: false,
       }
